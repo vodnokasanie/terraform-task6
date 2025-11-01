58683a9b750af66ca9463cd0a548ba26890df4df
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-11-02 00:25:04 +0600

    M

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-state-bucket"
-        key = "envs/dev/terraform.tfstate"
-        region = "us-east-1"
-        encrypt = true
-        dynamodb_table = "terraform-locks"
-    }
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
